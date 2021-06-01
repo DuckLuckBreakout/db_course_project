f9a0ad78eae89aad3fa86300c5f4d033b00cd402
DuckLuckBreakout <49189299+DuckLuckBreakout@users.noreply.github.com>
2021-06-01 22:16:11 +0300

    DCP-4 Change echo server to http server

--- cmd/main.go
+++ cmd/main.go
@@ -1,27 +1,21 @@
 package main
 
 import (
-	"github.com/labstack/echo/v4"
-	"github.com/labstack/echo/v4/middleware"
+	"log"
 	"net/http"
+	"time"
 )
 
+
 func main() {
-	// Echo instance
-	e := echo.New()
+	server := &http.Server{
+		Addr:           ":8080",
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+	}
 
-	// Middleware
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-
-	// Routes
-	e.GET("/", hello)
-
-	// Start server
-	e.Logger.Fatal(e.Start(":1323"))
-}
-
-// Handler
-func hello(c echo.Context) error {
-	return c.String(http.StatusOK, "Hello, World!")
+	if err := server.ListenAndServe(); err != nil {
+		log.Fatal(err)
+	}
 }
