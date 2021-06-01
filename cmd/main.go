3cef3ce18decc5f4ca5f78f5bccac88d8d73a13c
DuckLuckBreakout <49189299+DuckLuckBreakout@users.noreply.github.com>
2021-06-01 22:57:16 +0300

    DCP-11 Add connect to db

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"database/sql"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"time"
 )
 
 func main() {
+	postgreSqlConn, err := sql.Open(
+		"postgres",
+		"user=forum_root "+
+			"password=root "+
+			"dbname=forum "+
+			"host=localhost "+
+			"port= 5432"+
+			"sslmode=disable ",
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer postgreSqlConn.Close()
+
 	server := &http.Server{
 		Addr:         ":8080",
 		WriteTimeout: time.Second * 15,
